6305827b79da0941c9199c25d3247c5bba625313
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-10-01 13:50:23 -0300

    Removendo dependencias dos arquivos excluídos no commit passado.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/global.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Now its sfds
         </p>
         <a
           className="App-link"
