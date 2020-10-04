7dc2bcc617cf55de6cfef33880a08820980aa5ad
Lincon <linconsilva08@gmail.com>
2020-10-04 12:14:27 -0300

    Adicionando dependência react-router-dom
    Implementando algumas rotas para ver o funcionamento das rotas.

---
+++ src/pages/Feed/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Feed = () => {
+    return (
+        <h1>This is Feed</h1>
+    );
+};
+
+export default Feed;
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route , Switch} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Feed from './pages/Feed';
+import NotFound from './pages/NotFound';
 
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact/>
+            <Switch>
+                <Route path="/" component={Home} exact/>
+                <Route path="/feed" component={Feed} />
+                <Route component={NotFound}/>
+            </Switch>
         </BrowserRouter>
     )
 }
