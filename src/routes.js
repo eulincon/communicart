a1f416cb38b37089f288ba3112702d45b7533e83
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-10-07 16:57:45 -0300

    Criação VagaPage e VagaDetails componet.
    Mais algumas alterações básicas referênte à CSS global e modular em alguns componentes.

--- src/routes.js
+++ src/routes.js
@@ -6,14 +6,16 @@ import Feed from './pages/Feed';
 import NotFoundPage from './pages/NotFoundPage';
 import PageDefault from './pages/PageDefaullt';
 import Payments from './pages/Payments';
+import VagaPage from './pages/VagaPage';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" component={Home} exact/>
-                <Route path="/feed" component={Feed} />
-                <Route path="/page-default" component={PageDefault}/>
+                <Route path="/feed" component={PageDefault} />
+                <Route path="/page-default" component={Feed} />
+                <Route path="/vagas/1" component={VagaPage}/> 
                 <Route path="/payments" component={Payments}/>
                 <Route component={NotFoundPage} />
                 {/* <Redirect to="/404"/> */}
