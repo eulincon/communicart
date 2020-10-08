7ba3595f94d17c65b6159e9436e03ec979714e05
Arthur Dias <arthurdias.trad@outlook.com>
2020-10-08 20:02:58 -0300

    Página de perfil do usuário criada

---
+++ src/pages/UserProfile/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+import Main from "../../components/Main";
+
+const UserProfile = () => {
+  return <div></div>;
+};
+
+export default UserProfile;
