bbe9b5c44ce76e931a98f5d29a8d356bd7debf47
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-10-08 15:12:07 -0300

    Adicionando CardVaga

--- src/components/CardVaga/index.js
+++ src/components/CardVaga/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import * as AiIcons from 'react-icons/ai';
+import Jump from 'react-reveal/Jump';
+
+import styles from './styles.module.css';
 
 const CardVaga = () => {
+    const [likeHeart, setLikeHeart] = useState(false);
+
+    const like = () => {
+        setLikeHeart(!likeHeart);
+    }
+
     return (
         <div className="card my-4">
             <div className="card-body">
                 <h4 className="card-title">Título do anúncio</h4>
                 <span>Nome da empresa</span>
                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <Link to="#" className="btn btn-primary">Ver mais</Link>
+                <Link to="/vagas/1" className={`btn btn-primary ${styles.btn_primary}`}>Ver mais</Link>
+                {likeHeart ?
+                    <div className="float-right">
+                        <Jump>
+                            <AiIcons.AiFillHeart onClick={like} size="3rem" color="red"  />
+                        </Jump>
+                    </div>
+                    :
+                    <AiIcons.AiOutlineHeart onClick={like} size="3rem" className="float-right" />
+                }
             </div>
         </div>
     );
