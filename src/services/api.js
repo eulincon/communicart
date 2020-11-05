bdc0f437bff722dadbeb1e96c9fc185fb896bd4b
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-11-05 16:48:02 -0300

    Trying to disable cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
-    // headers: {
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Content-Type': 'application/json',
-    // },
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    baseURL: 'http://localhost:8080/',
+    withCredentials: false,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    },
+    // headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 });
 
 export default api;
