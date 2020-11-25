a0b0974140ca3eed52c6fead8a2252f869da9271
Arthur Dias <arthurdias.trad@outlook.com>
2020-11-25 15:29:45 -0300

    Função de carregamento de vaga por id

--- src/pages/VagaPage/index.js
+++ src/pages/VagaPage/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
 
-import SkeletonPage from '../../components/SkeletonPage';
-import VagaDetails from '../../components/VagaDetails';
+import SkeletonPage from "../../components/SkeletonPage";
+import VagaDetails from "../../components/VagaDetails";
+import Loading from "../../components/Loading";
 
 const VagaPage = () => {
-    return (
-        <SkeletonPage sidebar={true} footer={true}>
-            <h1 className="text-center text-white">Detalhes da vaga</h1>
-            <VagaDetails />
-        </SkeletonPage>
-    );
+  const [loading, setLoading] = useState(true);
+  const [vaga, setVaga] = useState({});
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function carregarVaga(vagaId) {
+      api.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
+        "@RNAuth:token"
+      )}`;
+      await api
+        .get(`/api/vagas/${vagaId}`)
+        .then((res) => {
+          setVaga({ ...res.data });
+          setLoading(false);
+        })
+        .catch((err) => {
+          if (err.response.status === 404) {
+            setLoading(false);
+            setVaga(null);
+          }
+        });
+    }
+    carregarVaga(id);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <SkeletonPage sidebar={true} footer={true}>
+      <h1 className="text-center text-white">Detalhes da vaga</h1>
+      <VagaDetails vaga={vaga} />
+    </SkeletonPage>
+  );
 };
 
 export default VagaPage;
