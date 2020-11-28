2f2f5213276cb9d6aaeea58382640dc2590ada4e
Arthur Dias <arthurdias.trad@outlook.com>
2020-11-27 22:14:39 -0300

    Criada página para mostrar perfil do usuário

--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -5,8 +5,7 @@ import api from "../../services/api";
 import { validarCNPJ, validarCPF } from "../../services/validation";
 
 import { useAuth } from "../../contexts/auth";
-import Footer from "../../components/Footer";
-import Menu from "../../components/Menu";
+import SkeletonPage from "../../components/SkeletonPage";
 
 const Registration = () => {
   const { user } = useAuth();
@@ -80,132 +79,130 @@ const Registration = () => {
 
   return (
     <>
-      <header>
-        <Menu />
-      </header>
-      <div className="py-5 full-cadastro text-white">
-        <div className="container bg-lighter_ py-3 px-5 form-box">
-          <h1 className="text-center">Cadastro</h1>
-          <form id="cadastro" className="mb-3" onSubmit={handleSubmit}>
-            <div className="form-group">
-              <div className="form-check form-check-inline mr-5">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="tipoCadastro"
-                  id="cadastro-cpf"
-                  value={0}
-                  defaultChecked="true"
-                  onChange={handleRadio}
-                />
-                <label className="form-check-label" htmlFor="inlineRadio1">
-                  Pessoa Física
-                </label>
-              </div>
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="tipoCadastro"
-                  id="cadastro-cnpj"
-                  value={1}
-                  onChange={handleRadio}
-                  required
-                />
-                <label className="form-check-label" htmlFor="inlineRadio2">
-                  Pessoa Jurídica
-                </label>
-              </div>
-            </div>
-            <div className="form-group mb-5">
-              {cadastro.tipoPessoa === 0 ? (
-                <>
-                  <label htmlFor="nome-pessoa-fisica">Nome*</label>
+      <SkeletonPage footer={true}>
+        <div className="py-5 full-cadastro text-white">
+          <div className="container bg-lighter_ py-3 px-5 form-box">
+            <h1 className="text-center">Cadastro</h1>
+            <form id="cadastro" className="mb-3" onSubmit={handleSubmit}>
+              <div className="form-group">
+                <div className="form-check form-check-inline mr-5">
                   <input
-                    type="text"
-                    className="form-control mb-4"
-                    id="nome-pessoa-fisica"
-                    placeholder="Insira seu nome"
-                    onChange={handleChange}
-                    value={cadastro.nome}
-                    name="nome"
-                    required
+                    className="form-check-input"
+                    type="radio"
+                    name="tipoCadastro"
+                    id="cadastro-cpf"
+                    value={0}
+                    defaultChecked="true"
+                    onChange={handleRadio}
                   />
-                  <label htmlFor="sobrenome-pessoa-fisica">Sobrenome*</label>
-                  <input
-                    type="text"
-                    className="form-control mb-4"
-                    id="sobrenome-pessoa-fisica"
-                    placeholder="Insira seu sobrenome"
-                    onChange={handleChange}
-                    value={cadastro.sobrenome}
-                    name="sobrenome"
-                    required
-                  />
-                  <label htmlFor="cpf-pessoa-fisica">CPF*</label>
-                  <input
-                    type="text"
-                    className="form-control mb-4"
-                    id="cpf-pessoa-fisica"
-                    placeholder="Insira seu CPF"
-                    onChange={handleChange}
-                    value={cpfMask(cadastro.cpf)}
-                    name="cpf"
-                    required
-                  />
-                </>
-              ) : (
-                <>
-                  <label htmlFor="nome-fantasia-PJ">Nome fantasia*</label>
-                  <input
-                    type="text"
-                    className="form-control mb-4"
-                    id="nome-fantasia-PJ"
-                    placeholder="Insira o nome fantasia da empresa"
-                    onChange={handleChange}
-                    value={cadastro.nomeFantasia}
-                    name="nomeFantasia"
-                    required
-                  />
-                  <label htmlFor="nome-representante-PJ">
-                    Nome do representante*
+                  <label className="form-check-label" htmlFor="inlineRadio1">
+                    Pessoa Física
                   </label>
+                </div>
+                <div className="form-check form-check-inline">
                   <input
-                    type="text"
-                    className="form-control mb-4"
-                    id="nome-representante-PJ"
-                    placeholder="Insira o nome do representante"
-                    onChange={handleChange}
-                    value={cadastro.nomeRepresentante}
-                    name="nomeRepresentante"
+                    className="form-check-input"
+                    type="radio"
+                    name="tipoCadastro"
+                    id="cadastro-cnpj"
+                    value={1}
+                    onChange={handleRadio}
                     required
                   />
-                  <label htmlFor="sobrenome-pessoa-fisica">CNPJ*</label>
-                  <input
-                    type="text"
-                    className="form-control mb-4"
-                    id="cnpj-pj"
-                    placeholder="Insira o CNPJ"
-                    onChange={handleChange}
-                    value={cnpjMask(cadastro.cnpj)}
-                    name="cnpj"
-                    required
-                  />
-                </>
-              )}
-            </div>
-            <div className="d-flex mt-5">
-              <button
-                className="text-center bg-dark px-4 py-2 btn-cadastro txt-secondary_"
-                type="submit"
-              >
-                Avançar
-              </button>
-            </div>
-          </form>
+                  <label className="form-check-label" htmlFor="inlineRadio2">
+                    Pessoa Jurídica
+                  </label>
+                </div>
+              </div>
+              <div className="form-group mb-5">
+                {cadastro.tipoPessoa === 0 ? (
+                  <>
+                    <label htmlFor="nome-pessoa-fisica">Nome*</label>
+                    <input
+                      type="text"
+                      className="form-control mb-4"
+                      id="nome-pessoa-fisica"
+                      placeholder="Insira seu nome"
+                      onChange={handleChange}
+                      value={cadastro.nome}
+                      name="nome"
+                      required
+                    />
+                    <label htmlFor="sobrenome-pessoa-fisica">Sobrenome*</label>
+                    <input
+                      type="text"
+                      className="form-control mb-4"
+                      id="sobrenome-pessoa-fisica"
+                      placeholder="Insira seu sobrenome"
+                      onChange={handleChange}
+                      value={cadastro.sobrenome}
+                      name="sobrenome"
+                      required
+                    />
+                    <label htmlFor="cpf-pessoa-fisica">CPF*</label>
+                    <input
+                      type="text"
+                      className="form-control mb-4"
+                      id="cpf-pessoa-fisica"
+                      placeholder="Insira seu CPF"
+                      onChange={handleChange}
+                      value={cpfMask(cadastro.cpf)}
+                      name="cpf"
+                      required
+                    />
+                  </>
+                ) : (
+                  <>
+                    <label htmlFor="nome-fantasia-PJ">Nome fantasia*</label>
+                    <input
+                      type="text"
+                      className="form-control mb-4"
+                      id="nome-fantasia-PJ"
+                      placeholder="Insira o nome fantasia da empresa"
+                      onChange={handleChange}
+                      value={cadastro.nomeFantasia}
+                      name="nomeFantasia"
+                      required
+                    />
+                    <label htmlFor="nome-representante-PJ">
+                      Nome do representante*
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control mb-4"
+                      id="nome-representante-PJ"
+                      placeholder="Insira o nome do representante"
+                      onChange={handleChange}
+                      value={cadastro.nomeRepresentante}
+                      name="nomeRepresentante"
+                      required
+                    />
+                    <label htmlFor="sobrenome-pessoa-fisica">CNPJ*</label>
+                    <input
+                      type="text"
+                      className="form-control mb-4"
+                      id="cnpj-pj"
+                      placeholder="Insira o CNPJ"
+                      onChange={handleChange}
+                      value={cnpjMask(cadastro.cnpj)}
+                      name="cnpj"
+                      required
+                    />
+                  </>
+                )}
+              </div>
+              <div className="d-flex mt-5">
+                <button
+                  className="text-center bg-dark px-4 py-2 btn-cadastro txt-secondary_"
+                  type="submit"
+                >
+                  Avançar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-      <Footer id="cadastro-footer" />
+      </SkeletonPage>
     </>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -42,13 +42,19 @@ function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/login" exact component={Login} />
         <Route path="/recuperacao" exact component={PasswordRecovery} />
         <Route path="/novaSenha" exact component={PasswordRegistration} />
         <Route path="/home" component={Home} exact />
         <PrivateRoute path="/feed" component={Feed} />
         {/* <Route path="/page-default" component={Feed} /> */}
         <PrivateRoute path="/vagas/:id" component={VagaPage} />
-        <Route path="/usuarios/1" component={UserProfile} />
+        <PrivateRoute
+          exact
+          path="/usuarios/meu-perfil"
+          component={UserProfile}
+        />
+        <PrivateRoute path="/usuarios/:id" component={UserProfile} />
         <Route path="/payments" component={Payments} />
         <Route path="/notify" component={Notify} />
         <Route path="/cadastro" component={Registration} />
